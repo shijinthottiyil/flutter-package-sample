26308f3438f02a54e6f60713187a4dd87c78e9b0
shijin thottiyil <115061533+shijinthottiyil@users.noreply.github.com>
2023-04-01 10:47:15 +0530

    Made some changes

--- lib/flutter_pkg.dart
+++ lib/flutter_pkg.dart
@@ -1,7 +1,30 @@
 library flutter_pkg;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
+import 'package:flutter/material.dart';
+
+class CustomButton extends StatelessWidget {
+  final VoidCallback onPressed;
+  final Widget child;
+  const CustomButton({
+    super.key,
+    required this.onPressed,
+    required this.child,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return TextButton(
+      onPressed: onPressed,
+      style: TextButton.styleFrom(
+        padding: const EdgeInsets.all(16.0),
+        foregroundColor: Colors.white,
+        backgroundColor: Colors.blue,
+        elevation: 9.0,
+        textStyle: const TextStyle(
+          fontSize: 20,
+        ),
+      ),
+      child: child,
+    );
+  }
 }
